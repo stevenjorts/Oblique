99ba7c1477b144b7061e164d16b287848d613878
stevenjorts <ortss623@newschool.edu>
2017-05-15 17:21:32 +0200

    New additions

---
+++ script.js
@@ -0,0 +1,13 @@
+function ResponseY() {
+    document.getElementById("demo").innerHTML = "Okay then, have a nice day.";
+}
+function ResponseN() {
+    document.getElementById("demo").innerHTML = "Have you gone swimming recently?(This would be a random JS generated question)";
+}
+
+function GetValue()
+{
+    var myarray= new Array("Have you swam recently?","Did you visit any museums recently?","Is talking to a machine going to help you?");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+   document.getElementById("message").innerHTML=random;
+}
